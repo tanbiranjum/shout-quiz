b303ec48619552687b096ad2dcd41b2e73999669
tanbiranjum <tanbiranjum@hotmail.com>
2022-10-11 02:28:44 +0600

    quiz validation added

--- src/components/QuizCard/QuizCard.js
+++ src/components/QuizCard/QuizCard.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import parse from "html-react-parser";
 
 const QuizCard = ({ info }) => {
-  return <div className="bg-slate-100">
-    {info.question}
-  </div>;
+  const [selected, setSelected] = useState();
+
+  const handleSelect = (question) => {
+    if (selected === question && selected === info.correctAnswer) {
+      return "bg-green-400";
+    } else if (selected === question && selected !== info.correctAnswer) {
+      return "bg-red-400";
+    } else if (question === info.correctAnswer) {
+      return "bg-green-400";
+    }
+  };
+
+  return (
+    <div className="bg-slate-100 mt-4 py-8">
+      <div className="p-6">{parse(`${info.question}`)}</div>
+      <div className="grid grid-cols-2 gap-4 p-2">
+        {info.options.map((question, id) => (
+          <button
+            key={id}
+            className={`bg-white p-4 cursor-pointer ${
+              selected && handleSelect(question)
+            }`}
+            onClick={(e) => {
+              setSelected(question);
+            }}
+            disabled={selected}
+          >
+            <p className="text-left">{question}</p>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default QuizCard;
