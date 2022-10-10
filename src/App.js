5d2e96dc19b20fb7b5d1dbf8de7dceb8ff3a7cdd
tanbiranjum <tanbiranjum@hotmail.com>
2022-10-10 21:46:41 +0600

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
+const router = createBrowserRouter([]);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
