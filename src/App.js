eff9482ff97bda33c30ba683b70759802ae5a171
tanbiranjum <tanbiranjum@hotmail.com>
2022-10-10 22:41:44 +0600

    nav added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
+import Main from "./layout/Main";
+import Blog from "./pages/Blog/Blog";
+import Home from "./pages/Home/Home";
+import Quiz from "./pages/Quiz/Quiz";
+import Stats from "./pages/Stats/Stats";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+        loader: async () => {
+          return fetch("https://openapi.programming-hero.com/api/quiz");
+        },
+      },
+      {
+        path: "blog",
+        element: <Blog />,
+      },
+      {
+        path: "stats",
+        element: <Stats />,
+      },
+      {
+        path: "quiz/:id",
+        element: <Quiz />,
+        loader: async ({ params }) => {
+          return fetch(
+            `https://openapi.programming-hero.com/api/quiz/${params.id}`
+          );
+        },
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="drop-shadow-md">
+      <div className="container flex justify-between px-8 py-6">
+        <h1 className="text-blue-700 font-extrabold text-4xl">Shout!</h1>
+        <div className="text-xl flex gap-6">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/stats">Stats</NavLink>
+          <NavLink to="/blog">Blog</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
