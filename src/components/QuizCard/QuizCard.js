cb49a4c735c6ba65bdd8e3857cd516a3eba84714
tanbiranjum <tanbiranjum@hotmail.com>
2022-10-11 12:08:38 +0600

    hover ui fixed for QuizCard

--- src/components/QuizCard/QuizCard.js
+++ src/components/QuizCard/QuizCard.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaEye } from "react-icons/fa";
 import parse from "html-react-parser";
 
 const QuizCard = ({ info }) => {
@@ -6,24 +7,33 @@ const QuizCard = ({ info }) => {
 
   const handleSelect = (question) => {
     if (selected === question && selected === info.correctAnswer) {
-      return "bg-green-400";
+      return "bg-green-700 text-white";
     } else if (selected === question && selected !== info.correctAnswer) {
-      return "bg-red-400";
+      return "bg-red-700 text-white";
     } else if (question === info.correctAnswer) {
-      return "bg-green-400";
+      return "bg-green-700 text-white";
     }
+  };
+
+  const handleShowCorrectAnswer = () => {
+    alert(info.correctAnswer);
   };
 
   return (
     <div className="bg-slate-100 mt-4 py-8">
-      <div className="p-6">{parse(`${info.question}`)}</div>
+      <div className="flex justify-between p-6">
+        <div>{parse(`${info.question}`)}</div>
+        <div onClick={handleShowCorrectAnswer}>
+          <FaEye className="cursor-pointer" />
+        </div>
+      </div>
       <div className="grid grid-cols-2 gap-4 p-2">
         {info.options.map((question, id) => (
           <button
             key={id}
             className={`border rounded p-4 cursor-pointer ${
               (selected && handleSelect(question)) || "bg-white"
-            }`}
+            } ${!selected && "hover:bg-blue-700 hover:text-white"}`}
             onClick={(e) => {
               setSelected(question);
             }}
