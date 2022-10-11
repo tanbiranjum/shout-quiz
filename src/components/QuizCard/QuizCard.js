2044086824dc44ad33023d28088234fea299975f
tanbiranjum <tanbiranjum@hotmail.com>
2022-10-11 14:11:57 +0600

    count functionality added

--- src/components/QuizCard/QuizCard.js
+++ src/components/QuizCard/QuizCard.js
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import { FaEye } from "react-icons/fa";
 import parse from "html-react-parser";
 
-const QuizCard = ({ info }) => {
+const QuizCard = ({
+  info,
+  rightCount,
+  setRightCount,
+  wrongCount,
+  setWrongCount,
+}) => {
   const [selected, setSelected] = useState();
 
   const handleSelect = (question) => {
@@ -19,10 +25,20 @@ const QuizCard = ({ info }) => {
     alert(info.correctAnswer);
   };
 
+  const handleCount = (question) => {
+    if (question === info.correctAnswer) {
+      setRightCount(rightCount + 1);
+    } else {
+      setWrongCount(wrongCount + 1);
+    }
+  };
+
   return (
     <div className="border-2 border-blue-700 mt-8 py-8 rounded drop-shadow-lg">
       <div className="flex justify-between p-6">
-        <p className="text-xl flex gap-2 text-blue-700 font-semibold">Question: {parse(`${info.question}`)}</p>
+        <p className="text-xl flex gap-2 text-blue-700 font-semibold">
+          Question: {parse(`${info.question}`)}
+        </p>
         <div onClick={handleShowCorrectAnswer}>
           <FaEye className="cursor-pointer text-blue-700 text-xl" />
         </div>
@@ -36,6 +52,7 @@ const QuizCard = ({ info }) => {
             } ${!selected && "hover:bg-blue-700 hover:text-white"}`}
             onClick={(e) => {
               setSelected(question);
+              handleCount(question);
             }}
             disabled={selected}
           >
--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -3,14 +3,25 @@ import { useLoaderData } from "react-router-dom";
 import QuizCard from "../../components/QuizCard/QuizCard";
 
 const Quiz = () => {
-  const [rightCount, setRightCount] = useState();
-  const [wrongCount, setWrongCount] = useState();
-  
+  const [rightCount, setRightCount] = useState(0);
+  const [wrongCount, setWrongCount] = useState(0);
+
   const quizes = useLoaderData();
   return (
     <div>
+      <div className="bg-blue-700 text-white p-4 text-lg rounded">
+        <p>Right Answer: {rightCount}</p>
+        <p>Wrong Answer: {wrongCount}</p>
+      </div>
       {quizes.data.questions.map((quiz) => (
-        <QuizCard key={quiz.id} info={quiz} />
+        <QuizCard
+          key={quiz.id}
+          info={quiz}
+          wrongCount={wrongCount}
+          setWrongCount={setWrongCount}
+          rightCount={rightCount}
+          setRightCount={setRightCount}
+        />
       ))}
     </div>
   );
