935918bfb6998d1751d8f8363cbddd05d4e1fafb
tanbiranjum <tanbiranjum@hotmail.com>
2022-10-11 13:42:11 +0600

    hero section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Card from "../../components/Card/Card";
+import Bg from "../../assets/bg.jpg";
 
 const Home = () => {
   const quizes = useLoaderData();
   return (
     <div>
-      <div className="grid grid-cols-4 gap-6">
+      <div className="bg-no-repeat bg-cover mt-4" style={{ backgroundImage: `url(${Bg})` }}>
+        <h3 className="text-7xl text-white font-extrabold p-24">
+          Improve Your <br />
+          Soft Skill
+        </h3>
+      </div>
+      <div className="grid grid-cols-4 gap-6 mt-8">
         {quizes.data.map((quiz) => (
           <Card key={quiz.id} info={quiz} />
         ))}
